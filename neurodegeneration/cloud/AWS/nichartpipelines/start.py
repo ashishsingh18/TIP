419a58128b5564a9f1b03a84eee7b9398d033f3f
Ashish Singh <ashish.singh@pennmedicine.upenn.edu>
2023-08-10 14:34:28 -0400

    Create zip archive of results and move it to S3 bucket

--- neurodegeneration/cloud/AWS/nichartpipelines/start.py
+++ neurodegeneration/cloud/AWS/nichartpipelines/start.py
@@ -7,20 +7,22 @@ import boto3
 import shutil
 
 #for testing
-#os.environ["AWS_BATCH_JOB_ID"] = "A" #auto populated
-#os.environ["JOB_INPUT_BUCKET"] = "from-kaapana"
-#os.environ["JOB_INPUT_KEY"] = "Subject1_0000_0000.nii.gz"
-#os.environ["JOB_REQUESTED_BY_USER"] = "kaapana"
-#os.environ['AWS_BATCH_JQ_NAME'] = "C" #auto populated
-#os.environ['AWS_BATCH_CE_NAME'] = "D" #auto populated
-#os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/sharedFolder/credentials"
+os.environ["AWS_BATCH_JOB_ID"] = "A" #auto populated
+os.environ["JOB_INPUT_BUCKET"] = "from-kaapana"
+os.environ["JOB_INPUT_KEY"] = "T1/Subjectc99cdb26803b4edfb757b2aa2c26084c1_0000_0000.nii.gz"
+os.environ["JOB_REQUESTED_BY_USER"] = "kaapana"
+os.environ['AWS_BATCH_JQ_NAME'] = "C" #auto populated
+os.environ['AWS_BATCH_CE_NAME'] = "D" #auto populated
+os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/sharedFolder/credentials"
 
-def get_seriesuid(bucket_name,object_key):
+def get_metadata(bucket_name,object_key):
   s3 = boto3.client('s3')
   object_metadata = s3.head_object(Bucket=bucket_name, Key=object_key)['Metadata']
   seriesuid = object_metadata['seriesuid']
+  series_description = object_metadata['seriesdescription']
   print("series uid = ", seriesuid)
-  return seriesuid
+  print(" series description = ", series_description)
+  return seriesuid,series_description
   
 
 def runProcessing():
@@ -68,54 +70,58 @@ s3 = boto3.client('s3')
 os.makedirs("/input/", exist_ok=True)
 os.makedirs("/output/", exist_ok=True)
 
-downloaded_file = "/input/" + input_key
+#following won't work correctly if there are multiple folders in input_key,
+#but we won't specify multiple folders, so this should work for now
+#TODO: make this generic
+folder_name, file_name = os.path.split(input_key)
+print("folder name: ", folder_name, "file name: ", file_name)
 
-uid = get_seriesuid(input_bucket,input_key)
+downloaded_file = "/input/" + file_name
+
+seriesuid,series_description = get_metadata(input_bucket,input_key)
 # Download input file(s) from S3 and place in input
 # Placeholder while we figure out more complex batch-subject management
 try:
     with open(downloaded_file, 'wb') as f:
         s3.download_fileobj(input_bucket, input_key, f)
-    #response = s3.get_object(Bucket=input_bucket, Key=input_key)
-    #print("CONTENT TYPE: " + response['ContentType'])
 except Exception as e:
     print(e)
     print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(input_key, input_bucket))
     raise e
 
-# requestedByUser = os.environ["JOB_REQUESTED_BY_USER"]
-
-#output_dir = os.path.join("/output/", requestedByUser)
 output_dir = "/output/"
 
 runProcessing()
-# output_archive = os.path.join("/", requestedByUser, job_id)
-
-# shutil.make_archive(output_archive, 'zip', output_dir)
-# print(f"Made archive at {output_archive}.zip")
-# print(os.listdir("/"))
-print(os.listdir(output_dir))
 
 print("Sending to S3...")
 
+#output bucket name
 out_bucket = "to-kaapana"
-#object_name = os.path.join("/private/", requestedByUser, job_id +".zip").lstrip('/')
-basename = os.path.basename(input_key).split('_0000_0000.nii.gz',1)[0]
-#object_name = "Subject1_0000.nii.gz"
-object_name = basename + '_0000.nii.gz'
-file_to_upload = os.path.join("/output/relabeled_out",object_name)
 
+#create a zip archive containing all output
+basename = os.path.basename(input_key).split('_0000_0000.nii.gz',1)[0]
+output_archive = os.path.join("/", basename)
+shutil.make_archive(output_archive, 'zip', output_dir)
+print(f"Made archive at {output_archive}.zip")
+
+#create object name and file to upload
+object_name = input_key.split('_0000_0000.nii.gz',1)[0] + '.zip'
+file_to_upload = output_archive + ".zip"
+print("file to upload: ", file_to_upload)
+print('object name: ', object_name)
+
+#upload to output bucket
 s3_upload = boto3.client('s3')
 print("Uploading file: ", file_to_upload)
 try:
     response = s3_upload.upload_file(
-        file_to_upload, out_bucket,input_key,
-        ExtraArgs={'Metadata': {'seriesuid': uid}}
+        file_to_upload, out_bucket,object_name,
+        ExtraArgs={'Metadata': {'seriesuid': seriesuid,'seriesdescription':series_description}}
     )
 except Exception as e:
     print(e)
     print('Error uploading object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(input_key, out_bucket))
     raise e
-print("Object should be available in bucket " + out_bucket + " with object key " + input_key)
+print("Object should be available in bucket " + out_bucket + " with object name " + object_name)
 print("Done.")
 
