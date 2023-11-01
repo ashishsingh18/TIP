b000467d4c2a12d1a15027d379c4d27784200ec0
Di Wu <WuDi2pennmedicine.upenn.edu>
2023-11-01 14:40:11 -0400

    1

--- neurodegeneration/processing-containers/biomarker_html_report_generator/files/main.py
+++ neurodegeneration/processing-containers/biomarker_html_report_generator/files/main.py
@@ -12,12 +12,12 @@ from html_generator.html_generator import _main as html_main
 def main(muse_roi, dlicv_mask,  wmls_mask, dcm_json):
     
     ## Create a tmp directory to store the intermediate results
-    # tmp_dir = 'tmp'
+    tmp_dir = 'tmp'
     
-    # if os.path.exists(tmp_dir):
-    #     shutil.rmtree(tmp_dir)
+    if os.path.exists(tmp_dir):
+        shutil.rmtree(tmp_dir)
         
-    # os.mkdir(tmp_dir)
+    os.mkdir(tmp_dir)
     
     ######################## Put all the intermediate output into the out folder##########################
     #      [UID]_all_MuseROIs_name.pkl – A dictionary with {‘ROI Name’: ‘ROI volume’}                    #
@@ -30,14 +30,14 @@ def main(muse_roi, dlicv_mask,  wmls_mask, dcm_json):
     #      [UID]_WMLSref.pkl - WMLS Reference Information                                                #
     ######################################################################################################
     
-    #dfSub, dfRef, WMLSref, dfPat, allz_num, allz, all_MuseROIs_num, all_MuseROIs_name = roi_quantifier_main(muse_roi, dlicv_mask, wmls_mask, dcm_json, './tmp/tmp.pdf')
+    dfSub, dfRef, WMLSref, dfPat, allz_num, allz, all_MuseROIs_num, all_MuseROIs_name, MuseROI = roi_quantifier_main(muse_roi, dlicv_mask, wmls_mask, dcm_json, './tmp/tmp.pdf')
     
     ######################## Put all the intermediate output into the tmp folder##########################
     #      [UID]_spareAD.pkl – SPARE AD score (single value)                                             #
     #      [UID]_spareBA.pkl - SPARE BA score (single value)                                             #
     ######################################################################################################
     
-    #spareAD, spareBA = spare_main(dfSub, all_MuseROIs_num, './out/out.pdf')
+    spareAD, spareBA = spare_main(dfSub, MuseROI, './tmp/tmp.pdf')
     
     ######################## Put all the intermediate output into the out folder##########################
     #      [UID]_flagtable.pkl –  html file contains table information (Brain Volumetry and Comparison   #
@@ -59,13 +59,13 @@ def main(muse_roi, dlicv_mask,  wmls_mask, dcm_json):
     # dfSub = pd.read_pickle('./tmp/tmp_dfSub.pkl')
     # dfRef = pd.read_pickle('./tmp/tmp_dfRef.pkl')
     # WMLSref = pd.read_pickle('./tmp/tmp_WMLSref.pkl')
-    allz_num = pd.read_pickle('./tmp/tmp_allz_num.pkl')
+    # allz_num = pd.read_pickle('./tmp/tmp_allz_num.pkl')
     # allz = pd.read_pickle('./tmp/tmp_allz.pkl')
     # all_MuseROIs_name = pd.read_pickle('./tmp/tmp_all_MuseROIs_name.pkl')
     # spareAD = pd.read_pickle('./tmp/tmp_spareAD.pkl')
     # spareBA = pd.read_pickle('./tmp/tmp_spareBA.pkl')
     
-    #biomarker_main(dfSub, dfRef, WMLSref, allz_num, allz, all_MuseROIs_name, spareAD, spareBA, './tmp/tmp.pdf')
+    biomarker_main(dfSub, dfRef, WMLSref, allz_num, allz, all_MuseROIs_name, spareAD, spareBA, './tmp/tmp.pdf')
     
     ############################ Brain Visualize ########################################################
     
@@ -74,27 +74,27 @@ def main(muse_roi, dlicv_mask,  wmls_mask, dcm_json):
 
     ############################ CVS Extraction #########################################################
     
-    #csv_main(dfSub, dfRef, WMLSref, allz_num, allz, all_MuseROIs_name, spareAD, spareBA, './tmp/tmp.pdf')
+    csv_main(dfSub, dfRef, WMLSref, allz_num, allz, all_MuseROIs_name, spareAD, spareBA, './tmp/tmp.pdf')
     
     ############################ html extraction ########################################################
     
-    # dfPat = pd.read_pickle('./tmp/tmp_dfPat.pkl')
-    # table = pd.read_pickle('./tmp/tmp_roisubsettable.pkl')
-    # flagtable = pd.read_pickle('./tmp/tmp_flagtable.pkl')
+    dfPat = pd.read_pickle('./tmp/tmp_dfPat.pkl')
+    table = pd.read_pickle('./tmp/tmp_roisubsettable.pkl')
+    flagtable = pd.read_pickle('./tmp/tmp_flagtable.pkl')
 
-    # absolute_path = os.path.abspath(os.getcwd())
+    absolute_path = os.path.abspath(os.getcwd())
 
-    # pdf_path = absolute_path + '/tmp/tmp.pdf'
-    # html_main(pdf_path, dfPat, table, flagtable)
+    pdf_path = absolute_path + '/tmp/tmp.pdf'
+    html_main(pdf_path, dfPat, table, flagtable)
 
     #output is a single html file + all files required by csv_extraction container
     
     
 if __name__ == '__main__':
 
-    roi = ['/home/diwu/Desktop/kaapana dataset/5datasets_F8F12F18M13M5/F8/batch/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328/extract_muse_result/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328.nii.gz']
-    icv = ['/home/diwu/Desktop/kaapana dataset/5datasets_F8F12F18M13M5/F8/batch/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328/extract_dlicv_result/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328.nii.gz']
-    wmls = ['/home/diwu/Desktop/kaapana dataset/5datasets_F8F12F18M13M5/F8/batch/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328/wmls-output/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328.nii.gz']
-    _json = '/home/diwu/Desktop/kaapana dataset/5datasets_F8F12F18M13M5/F8/batch/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328/GetT1Metadata/2.16.840.1.114362.1.12066432.24920037488.604832630.1009.6328.json'
+    roi = ['/home/diwu/Desktop/F8_F12_M2_M3_M5/M2/batch/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888/extract_muse_lps/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888.nii.gz']
+    icv = ['/home/diwu/Desktop/F8_F12_M2_M3_M5/M2/batch/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888/extract_dlicv_result/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888.nii.gz']
+    wmls = ['/home/diwu/Desktop/F8_F12_M2_M3_M5/M2/batch/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888/wmls-output/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888.nii.gz']
+    _json = '/home/diwu/Desktop/F8_F12_M2_M3_M5/M2/batch/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888/GetT1Metadata/2.16.840.1.114362.1.12066432.24920037488.604832288.140.888.json'
     
     main(roi, icv, wmls, _json)
